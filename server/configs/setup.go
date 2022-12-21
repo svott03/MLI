0b2186c3afed77e603d9d35ff6813b38f6bf4444
Steve Vott <svott@usc.edu>
2022-12-21 15:42:26 -0800

    MongoDB files imported

--- server/configs/setup.go
+++ server/configs/setup.go
@@ -32,6 +32,6 @@ var DB *mongo.Client = ConnectDB()
 
 // getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	collection := client.Database("golangAPI").Collection(collectionName)
+	collection := client.Database("MyDatabase").Collection(collectionName)
 	return collection
 }
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -7,9 +7,6 @@ import (
 
 func UserRoute(router *gin.Engine) {
 	router.POST("/user", controllers.CreateUser())
-	router.GET("/user/:userId", controllers.GetAUser()) 
-	router.PUT("/user/:userId", controllers.EditAUser())
-	router.DELETE("/user/:userId", controllers.DeleteAUser())
 	router.GET("/users", controllers.GetAllUsers())
 	router.GET("/", controllers.GetMainPage())
 	router.GET("/train", controllers.Train())
